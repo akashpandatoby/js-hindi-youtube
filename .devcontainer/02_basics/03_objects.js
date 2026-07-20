667d92ed2b33380ae109b9ad5d7a899b53f4d3e9
akashpandatoby <akashpanda366@gmail.com>
2026-07-20 10:45:51 +0530

    completed Javascript objects lesson

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,14 @@
+//  singleton
+// object.create
+
+// object literals
+const JsUser = {
+    name: "Akash",
+    age: 18,
+    location: "kanpur",
+    email: "akash@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser.email)
+console.log(JsUser["email"])
